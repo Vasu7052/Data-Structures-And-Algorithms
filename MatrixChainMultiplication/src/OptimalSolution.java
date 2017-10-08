3c167df36079648a5339d875d3411c9971ab0900
Vasu Gupta <androfly.official@gmail.com>
2017-10-08 23:15:46 +0530

    DS ALGO

--- MatrixChainMultiplication/src/OptimalSolution.java
+++ MatrixChainMultiplication/src/OptimalSolution.java
@@ -1,6 +1,67 @@
 
 public class OptimalSolution {
 	
+	static int[] matrices = {4,10,3,12,20,7} ;
+	
+	public static void main(String[] args) {
+		
+		int len1 = matrices.length ;
+		
+		int[][] solution = new int[len1-1][len1-1] ;
+		
+		for(int i = 0 ; i < solution.length ; i++) {
+			for(int j = 0 ; j < solution.length ; j++) {
+				solution[i][j] = 0 ;
+			}
+		}
+		
+		
+		for (int i = 0 ; i < 4 ; i++) {
+			
+			for (int j = 0; j < solution.length - 1; j++) {
+				
+				for (int k = i+j+1 ; k < solution.length ; k++) {
+					
+					if(k == i+j+1) {
+						solution[j][k] = findMinimum(solution , j , k);
+					}
+					
+				}
+				
+				
+			}
+		}
+		
+		
+		for (int i = 0; i < solution.length; i++) {
+			for (int j = 0; j < solution.length; j++) {
+				System.out.print(solution[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Minimum number of multiplications : " + solution[0][solution.length-1]);
+		
+		
+	}
+	
+	public static int findMinimum(int[][] array , int i , int j){
+		
+		int min = Integer.MAX_VALUE ;
+		
+		for (int k = i ; k < j ; k++) {
+			
+			int temp = array[i][k] + array[k+1][j] + (matrices[i]*matrices[k+1]*matrices[j+1]) ;
+			
+			if(temp <= min) {
+				min = temp ;
+			}
+		}
+		
+		return min ;
+		
+	}
+	
 	
 
 }
