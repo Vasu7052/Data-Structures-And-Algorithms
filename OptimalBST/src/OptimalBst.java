0f95cf726ea42ddac220ee2717d56dafd5862b88
Vasu Gupta <androfly.official@gmail.com>
2017-10-08 23:38:43 +0530

    DS ALGO

--- OptimalBST/src/OptimalBst.java
+++ OptimalBST/src/OptimalBst.java
@@ -26,6 +26,22 @@ public class OptimalBst {
 			}
 		}
 		
+		for (int i = 0 ; i < solution.length-1 ; i++) {
+			
+			for (int j = 0; j < solution.length - 1; j++) {
+				
+				for (int k = i+j+1 ; k < solution.length ; k++) {
+					
+					if(k == i+j+1) {
+						solution[j][k] = findMinimum(solution , j , k);
+					}
+					
+				}
+				
+				
+			}
+		}
+		
 		for (int i = 0; i < solution.length; i++) {
 			for (int j = 0; j < solution.length; j++) {
 				System.out.print(solution[i][j] + " ");
@@ -33,8 +49,24 @@ public class OptimalBst {
 			System.out.println();
 		}
 		
+	}
+	
+	public static int findMinimum(int[][] array , int i , int j){
 		
+		int min = Integer.MAX_VALUE ;
+		
+		for (int k = i ; k < j ; k++) {
+			
+			int temp = array[i][k] + array[k+1][j] + (matrices[i]*matrices[k+1]*matrices[j+1]) ;
+			
+			if(temp <= min) {
+				min = temp ;
+			}
+		}
+		
+		return min ;
 		
 	}
+	
 
 }
