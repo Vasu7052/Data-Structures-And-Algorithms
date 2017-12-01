3e51613e1aee15d1ecf602596c4f321547221963
Vasu Gupta <androfly.official@gmail.com>
2017-12-01 14:52:42 +0530

    Genetic Algorithm

--- Genetic Algorithm/src/Main.java
+++ Genetic Algorithm/src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         //Calculate fitness of each individual
         demo.population.calculateFitness();
 
-        System.out.println("one Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
+        System.out.println("Start => Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
 
         //While population gets an individual with maximum fitness
         while (demo.population.fittest < 5) {
@@ -138,96 +138,5 @@ public class Main {
 }
 
 
-//Individual class
-class Individual {
 
-    int fitness = 0;
-    int[] genes = new int[5];
-    int geneLength = 5;
 
-    public Individual() {
-        Random rn = new Random();
-
-        //Set genes randomly for each individual
-        for (int i = 0; i < genes.length; i++) {
-            genes[i] = rn.nextInt() % 2;
-        }
-
-        fitness = 0;
-    }
-
-    //Calculate fitness
-    public void calcFitness() {
-
-        fitness = 0;
-        for (int i = 0; i < 5; i++) {
-            if (genes[i] == 1) {
-                ++fitness;
-            }
-        }
-    }
-
-}
-
-//Population class
-class Population {
-
-    int popSize = 10;
-    Individual[] individuals = new Individual[10];
-    int fittest = 0;
-
-    //Initialize population
-    public void initializePopulation(int size) {
-        for (int i = 0; i < individuals.length; i++) {
-            individuals[i] = new Individual();
-        }
-    }
-
-    //Get the fittest individual
-    public Individual getFittest() {
-        int maxFit = Integer.MIN_VALUE;
-        for (int i = 0; i < individuals.length; i++) {
-            if (maxFit <= individuals[i].fitness) {
-                maxFit = i;
-            }
-        }
-        fittest = individuals[maxFit].fitness;
-        return individuals[maxFit];
-    }
-
-    //Get the second most fittest individual
-    public Individual getSecondFittest() {
-        int maxFit1 = 0;
-        int maxFit2 = 0;
-        for (int i = 0; i < individuals.length; i++) {
-            if (individuals[i].fitness > individuals[maxFit1].fitness) {
-                maxFit2 = maxFit1;
-                maxFit1 = i;
-            } else if (individuals[i].fitness > individuals[maxFit2].fitness) {
-                maxFit2 = i;
-            }
-        }
-        return individuals[maxFit2];
-    }
-
-    //Get index of least fittest individual
-    public int getLeastFittestIndex() {
-        int minFit = 0;
-        for (int i = 0; i < individuals.length; i++) {
-            if (minFit >= individuals[i].fitness) {
-                minFit = i;
-            }
-        }
-        return minFit;
-    }
-
-    //Calculate fitness of each individual
-    public void calculateFitness() {
-
-        for (int i = 0; i < individuals.length; i++) {
-            individuals[i].calcFitness();
-        }
-        getFittest();
-    }
-
-}
